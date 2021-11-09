959d421d4d928720999413a4ad509062719bfea6
Alan Casas <alan_casas_arevalo@carrefour.com>
2021-11-09 19:04:22 +0100

    NetworkingManager was implemented on coin data service

--- CryptoApp/Services/CoinDataService.swift
+++ CryptoApp/Services/CoinDataService.swift
@@ -6,36 +6,35 @@ class CoinDataService {
     var coinSubscription: AnyCancellable?
     @Published var allCoinError: Error? = nil
 
-    init () {
+    init() {
         getAllCoins()
     }
 
-    private func getAllCoins () {
-        guard let url = URL(string: "\(URLConstants.coingeckoBaseURL)?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h") else { return }
-
-        coinSubscription = URLSession.shared.dataTaskPublisher(for: url)
-        .subscribe(on: DispatchQueue.global(qos: .default))
-        .tryMap { [weak self] (output) -> Data in
-            guard let response = output.response as? HTTPURLResponse, response.statusCode >= 200 && response.statusCode < 300 else {
-                self?.allCoinError = URLError(.badServerResponse)
-                throw URLError(.badServerResponse)
-            }
-            return output.data
+    private func getAllCoins() {
+        guard let url = URL(string: "\(URLConstants.coingeckoBaseURL)?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h") else {
+            return
         }
-        .receive(on: DispatchQueue.main)
-        .decode(type: [CoinModel].self, decoder: JSONDecoder())
-        .sink(receiveCompletion: { [weak self] completion in
-            switch completion {
-            case .finished:
-                print("Finish")
-            case .failure(let error):
-                self?.allCoinError = error
-                print("Finish with Error: \(error.localizedDescription)")
-            }
-        }, receiveValue: { [weak self] allCoinsFromBack in
-            self?.allCoins = allCoinsFromBack
-            self?.coinSubscription?.cancel()
-        })
+
+        coinSubscription = NetworkingManager.download(from: url)
+                .decode(type: [CoinModel].self, decoder: JSONDecoder())
+                /*
+                This is other way to do if you want
+                 .sink(receiveCompletion: NetworkingManager.handleCompletion) { [weak self] allCoinsFromBack in
+                 self?.allCoins = allCoinsFromBack
+                 self?.coinSubscription?.cancel()
+                 }
+                 */
+                .sink(receiveCompletion: { [weak self] completion in
+                    switch completion {
+                    case .finished:
+                        print("Finish")
+                    case .failure(let error):
+                        self?.allCoinError = error
+                    }
+                }, receiveValue: { [weak self] allCoinsFromBack in
+                    self?.allCoins = allCoinsFromBack
+                    self?.coinSubscription?.cancel()
+                })
     }
 
 }
