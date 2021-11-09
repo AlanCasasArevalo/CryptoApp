4733117f71dd18886eede1c2d692eab561ff9976
Alan Casas <alan_casas_arevalo@carrefour.com>
2021-11-09 13:50:00 +0100

    Some text were added to our text constants

--- CryptoApp/Common/TextConstants.swift
+++ CryptoApp/Common/TextConstants.swift
@@ -3,4 +3,7 @@ import Foundation
 struct TextConstants {
     static let livePricesHeaderTitle = "Live prices"
     static let portfolioHeaderTitle = "Portfolio"
+    static let columnCoinHeaderTitle = "Coin"
+    static let columnHoldingsHeaderTitle = "Holdings"
+    static let columnPriceHeaderTitle = "Price"
 }
